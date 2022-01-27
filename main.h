2846bdca42b1e9e75996036d36d4978bc194c56f
Bibimarium <89059287+Bibimarium@users.noreply.github.com>
2022-01-27 19:47:58 +0530

    Update main.h

--- main.h
+++ main.h
@@ -1,25 +1,40 @@
-/* 
- * File:   main.h
- * Author: ALI
- *
- * Created on 14 December, 2021, 6:44 PM
- */
+//  File:   main.h
 
 #ifndef MAIN_H
-#define	MAIN_H
-
-#define COOKING_MODE_DISPLAY   0X01
-#define MICRO_MODE             0X02
-#define TIME_DISPLAY           0x03
-#define MICRO_MODE_RESET       0x10
-#define RESET_NOTHING          0X0F
-
+#define MAIN_H
+#define COOKING_MODE_DISPLAY    0x01
+#define MICRO_MODE              0x02
+#define GRILL_MODE              0x03
+#define CONVECTION_MODE         0x04
+#define START_MODE              0x05
+#define TIME_DISPLAY            0x06
+#define STOP                    0x07
+#define PAUSE                   0x08
+     
+#define MICRO_MODE_RESET        0X11
+#define RESET_NOTHING           0x0F
+#define GRILL_MODE_RESET        0x12
+#define RESET_TIME              0x13
+#define RESET_TEMP              0X14
+#define BUZZER                  RC1
+#define BUZZER_DDR              TRISC1
 #define FAN                     RC2
-#define FAN_DDR                 TRISC2
+#define FAN_DDR                 TRISC2 
+#define ON                      1
+#define OFF                     0
+#define DOOR                    RB3
+#define OPEN                    0
+#define CLOSED                  1
+#define FAILURE                 1
+#define SUCCESS                 0
+
 void power_on_screen(void);
 void cooking_mode_display(void);
-void clear_clcd(void);
-void clcd_write(unsigned char byte, unsigned char mode);
+void clear_display(void);
 void set_time(unsigned char key);
-#endif	/* MAIN_H */
+void opearation_call(unsigned char key);
+void time_display(void);
+char set_temp(unsigned char key);
+
+#endif
 
